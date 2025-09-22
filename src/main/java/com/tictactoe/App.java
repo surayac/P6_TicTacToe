5cf5ab57ed0c664d61cf87820b47ebbbab065534
surayac <surayacsm@gmail.com>
2025-09-22 12:27:42 +0200

    adding initial code

--- src/main/java/com/tictactoe/App.java
+++ src/main/java/com/tictactoe/App.java
@@ -1,13 +1,10 @@
 package com.tictactoe;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.tictactoe.Board;
+
+public class App {
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.printBoard();
     }
 }
---
+++ src/main/java/com/tictactoe/Board.java
@@ -0,0 +1,22 @@
+package com.tictactoe;
+
+public class Board {
+    char[][] board = {
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' }
+    };
+
+       public void printBoard(board) {
+        System.out.println("Tablero vacío:");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            System.out.print(" ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------");
+    }
+}
