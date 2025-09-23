11e8204806cd566a0052b9ffcf7f2eaede296f2b
sofiatoroviafara01 <sofiaviafara123@gmail.com>
2025-09-23 09:51:46 +0200

    Change code lines in board

--- src/main/java/com/tictactoe/Board.java
+++ src/main/java/com/tictactoe/Board.java
@@ -1,16 +1,22 @@
 package com.tictactoe;
 
 public class Board {
-    char[][] board = {
-            { ' ', ' ', ' ' },
-            { ' ', ' ', ' ' },
-            { ' ', ' ', ' ' }
-    };
-
-    public void printBoard() {
-        System.out.println("Tablero vacío:");
+    char[][] board;
+    public Board () {
+        board = new char[3][3];
+        clear();
+    
+    } 
+    private void clear() {
+        for (int i = 0;i <3; i++)
+            for (int j = 0;j <3;  j++)
+            board[i][j]=' ';
+    }
+    
+   public void printBoard() {
+        System.out.println("El tablero se compone de fila 0 - 1 y 2 de columna 0 -1 y 2:");
         for (int i = 0; i < 3; i++) {
-            System.out.println("-------------");
+            System.out.println("\t _ _ _");
             System.out.print(" ");
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
