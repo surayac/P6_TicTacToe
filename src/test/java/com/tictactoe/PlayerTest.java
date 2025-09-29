0d6d4add6e3821d719fcc33d187d7815d6b05405
surayac <surayacsm@gmail.com>
2025-09-29 10:18:26 +0200

    bugfix(PlayerTest): bugfix to clean lines of code

--- src/test/java/com/tictactoe/PlayerTest.java
+++ src/test/java/com/tictactoe/PlayerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Scanner;
@@ -29,26 +28,7 @@ public class PlayerTest {
     void testGetSymbol_to_check_if_symbol_is_assignated_to_player() {
         assertEquals('X', player.getSymbol());
     }
-<<<<<<< HEAD
 
-    @Test
-    void testGetColor_to_check_playerX_color() {
-        assertEquals("\033[0;35m", player.getColor());
-    }
-
-    @Test
-    void testPlayerTurn() {
-        assertArrayEquals(new int[] { 1, 2 }, player.playerTurn());
-    }
-
-    @Test
-    void testPlayerOColor() {
-        Player playerO = new Player('O', new Scanner("0\n0\n"));
-        assertEquals('O', playerO.getSymbol());
-        assertEquals("\033[0;36m", playerO.getColor());
-    }
-}
-=======
     @Test 
     void testGetColor() {
         Scanner fakeScanner = new Scanner ("");
@@ -59,4 +39,3 @@ public class PlayerTest {
         assertEquals("\033[0;35m", color);
     }
 }
->>>>>>> e183fd556c484b57fd0967eb81e013988af0ab61
